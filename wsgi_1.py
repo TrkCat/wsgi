6cb8615df091e06e6bc7b055607f670e27da9cac
Joel Rhode <joelrhode@gmail.com>
2020-05-02 21:06:50 -0500

    completed all content for lesson04 activities

--- wsgi_1.py
+++ wsgi_1.py
@@ -19,13 +19,16 @@ def application(environ, start_response):
     import pprint
     pprint.pprint(environ)
 
+    today = datetime.date.today()
+
+
     response_body = body.format(
         software=environ.get('SERVER_SOFTWARE', default),
-        path="aaaa",
-        month="bbbb",
-        date="cccc",
-        year="dddd",
-        client_ip="eeee"
+        path=environ.get('SCRIPT_NAME'),
+        month=today.strftime('%B'),
+        date=today.day,
+        year=today.year,
+        client_ip=environ.get('REMOTE_ADDR')
     )
     status = '200 OK'
 
